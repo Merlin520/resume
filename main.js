be9a03d8e8473f142b2260ecfa358f37940c204b
meiya <meiya90hou@163.com>
2018-06-04 21:13:05 +0800

    点击出现菜单

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ portfolio3.onclick = function () {
 //清除loading
 loadingPage.classList.remove('active');
 
-
+//滑动
 window.onscroll = function (xx) {
     if(window.scrollY > 0){
         topNavBar.classList.add('sticky')
@@ -23,5 +23,16 @@ window.onscroll = function (xx) {
     }
 };
 
+//点击下拉菜单效果
+let liTags = document.querySelectorAll('nav.navMenu > ul > li');
+for(let i = 0;i < liTags.length;i++){
+    liTags[i].onmouseenter = function (x) {
+        x.currentTarget.classList.add('active')
+    };
+    liTags[i].onmouseleave = function (x) {
+        x.currentTarget.classList.remove('active');
+
+    };
+}
 
 
