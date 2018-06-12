1987da8cdeccd695ffa4dded067013d12232fccf
meiya <meiya90hou@163.com>
2018-06-12 22:55:12 +0800

    留言初步展示

--- js/message.js
+++ js/message.js
@@ -6,6 +6,7 @@ AV.init({
     appKey: APP_KEY
 });
 
+//存数据
 let myForm = document.querySelector('#postMessageForm');
 
 myForm.addEventListener('submit',function (e) {
@@ -16,9 +17,31 @@ myForm.addEventListener('submit',function (e) {
     message.save({
     content: content
 }).then(function(object) {
-    alert('留言成功');
+    window.location.reload()
 })
 });
+
+//获取数据
+var query = new AV.Query('Message');
+query.find().then(function (messages) {
+    let array = messages.map((item)=>item.attributes);
+    array.forEach((item)=>{
+        let li = document.createElement('li');
+        li.innerText = item.content;
+        let messageList = document.querySelector('#messageList');
+        messageList.appendChild(li)
+    })
+}, function (error) {
+    alert('网络繁忙，稍后再试')
+})
+    .then(()=>{},(error)=>{
+        console.log(error);
+    });
+
+
+
+
+
 
 
 
