fdfe7cb28f3a9ae65a282b38a6b6f42ddc300fcc
meiya <meiya90hou@163.com>
2018-06-14 21:54:50 +0800

    修改bug

--- js/message.js
+++ js/message.js
@@ -65,10 +65,9 @@
 
             // let myForm = document.querySelector('#postMessageForm');
             //     let myForm = this.form;
-                this.form.addEventListener('submit',function (e) {
+                this.form.addEventListener('submit', (e)=> {
                 e.preventDefault();
-                this.saveMessage()
-
+                this.saveMessage();
             });
         },
 
